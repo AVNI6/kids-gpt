e816cd8e7a31f4f075b40bd99c0c56946e41483d
Urvish Vagdiya <urvish@gmail.com>
2026-05-01 09:43:28 +0530

    changes in navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Profile from "./profile/page";
+
 export default async function Home() {
   return (
-    <div>
+    <div className="flex justify-between">
       <div>Kids-GPT</div>
+      <Profile />
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,20 +1,49 @@
 "use client";
 import { createClient } from "@/lib/supabase/client";
+import { useState } from "react";
 
 const supabase = createClient();
 
 const Profile = () => {
+  const [profile, setProfile] = useState("");
+
+  const handleClick = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error(error);
+    }
+    setProfile("");
+  };
+
   const handleUser = async () => {
-    const { data } = await supabase.auth.getUser();
-    console.log(data);
+    const { data, error } = await supabase.auth.getUser();
+    if (error) {
+      console.error(error);
+    }
+    if (data?.user) {
+      setProfile(data.user.email || "");
+    }
   };
   return (
-    <button
-      onClick={() => handleUser()}
-      className="active:scale-95 hover:cursor-pointer bg-gray-600 text--white px-3 py-1 rounded-md"
-    >
-      Get User
-    </button>
+    <>
+      <div className="flex gap-4">
+        {profile && (
+          <div className="w-7 h-7 bg-black text-white rounded-full flex items-center justify-center">
+            {profile.charAt(0).toUpperCase()}
+          </div>
+        )}
+
+        {!profile && (
+          <button
+            onClick={() => handleUser()}
+            className="active:scale-95 hover:cursor-pointer bg-gray-600 text--white px-3 py-1 rounded-md"
+          >
+            Get User
+          </button>
+        )}
+        {profile && <div onClick={() => handleClick()}>Log Out</div>}
+      </div>
+    </>
   );
 };
 
