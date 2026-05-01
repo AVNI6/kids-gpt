5dfced42bd27a1007bdc84500ea9bd6e3337e9ab
Urvish Vagdiya <urvish@gmail.com>
2026-05-01 12:12:54 +0530

    changes in UI

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counterSlice";
+import counterReducer from "../store/slice/counterSlice";
 
 export const store = configureStore({
   reducer: {
