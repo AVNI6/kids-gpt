a1869e3717115c7f94a43c4f4a45678544f5df2c
Ayush231104 <ayushzalavadiya23112004@gmail.com>
2026-06-15 13:02:24 +0530

    loader

--- src/app/(kid)/page.tsx
+++ src/app/(kid)/page.tsx
@@ -2,15 +2,5 @@ import { Suspense } from "react";
 import ChatInterface from "@/components/shared/chat-interface/ChatInterface";
 
 export default async function Home() {
-  return (
-    <Suspense
-      fallback={
-        <div className="flex-1 flex items-center justify-center">
-          <div className="w-8 h-8 border-4 border-sky-500 border-t-transparent rounded-full animate-spin" />
-        </div>
-      }
-    >
-      <ChatInterface />
-    </Suspense>
-  );
+  return <ChatInterface />;
 }
